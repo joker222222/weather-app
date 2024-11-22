65d7ce3a35c0e120542efdf1eadc57d6e8e36e4e
joker222222 <57990119+joker222222@users.noreply.github.com>
2024-11-22 15:41:25 +0300

    small-change

--- js/locations.js
+++ js/locations.js
@@ -1,15 +1,26 @@
 let inputCity = document.querySelector('.search-input')
 let appContent = document.querySelectorAll('.content')
 let contentCity = document.querySelectorAll('.content-city-name')
+let noCity = document.querySelector('.not-found')
 
 inputCity.oninput = () => {
 	let currentCity = String(inputCity.value)
+	let countHiddenCity = 0
 	for (let i = 0; i < appContent.length; i++) {
 		let cityName = appContent[i].querySelector('.content-city-name').textContent
 		if (cityName.indexOf(String(currentCity)) >= 0) {
 			appContent[i].classList.remove('content-hidden')
 		} else {
+			countHiddenCity++
 			appContent[i].classList.add('content-hidden')
 		}
+	}
+	if (countHiddenCity == appContent.length) {
+		console.log(1)
+		noCity.textContent = 'Городов не найдено'
+		noCity.classList.add('weather-text')
+	} else {
+		noCity.textContent = ''
+		noCity.classList.remove('weather-text')
 	}
 }
