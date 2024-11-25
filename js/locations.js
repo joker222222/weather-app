174eb6320e990c83d7e3f4574f2deb9434cc1fbc
joker222222 <live.korpus@mail.ru>
2024-11-25 19:31:16 +0300

    add-search-all-register-and-btn-reset

--- js/locations.js
+++ js/locations.js
@@ -1,26 +1,54 @@
 let inputCity = document.querySelector('.search-input')
 let appContent = document.querySelectorAll('.content')
 let contentCity = document.querySelectorAll('.content-city-name')
-let noCity = document.querySelector('.not-found')
+let noCityText = document.querySelector('.not-found-text-hidden')
+let noCityBtn = document.querySelector('.not-found-btn-hidden')
 
-inputCity.oninput = () => {
-	let currentCity = String(inputCity.value)
+const checkAndSetCity = currentCity => {
 	let countHiddenCity = 0
 	for (let i = 0; i < appContent.length; i++) {
-		let cityName = appContent[i].querySelector('.content-city-name').textContent
-		if (cityName.indexOf(String(currentCity)) >= 0) {
+		let cityName = String(
+			appContent[i].querySelector('.content-city-name').textContent
+		).toLowerCase()
+		if (cityName.indexOf(currentCity) >= 0) {
 			appContent[i].classList.remove('content-hidden')
 		} else {
 			countHiddenCity++
 			appContent[i].classList.add('content-hidden')
 		}
 	}
+	return countHiddenCity
+}
+
+const setTextAndBtnNotFound = countHiddenCity => {
 	if (countHiddenCity == appContent.length) {
-		console.log(1)
-		noCity.textContent = 'Городов не найдено'
-		noCity.classList.add('weather-text')
+		noCityText.textContent = 'Городов не найдено'
+		noCityText.classList.add('not-found-text')
+		noCityText.classList.remove('not-found-text-hidden')
+
+		noCityBtn.classList.add('not-found-btn')
+		noCityBtn.classList.remove('not-found-btn-hidden')
 	} else {
-		noCity.textContent = ''
-		noCity.classList.remove('weather-text')
+		noCityText.textContent = ''
+		noCityText.classList.add('not-found-text-hidden')
+		noCityText.classList.remove('not-found-text')
+
+		noCityBtn.classList.add('not-found-btn-hidden')
+		noCityBtn.classList.remove('not-found-btn')
 	}
+}
+
+const startSearch = () => {
+	let currentCity = String(inputCity.value).toLowerCase()
+	countHiddenCity = checkAndSetCity(currentCity)
+	setTextAndBtnNotFound(countHiddenCity)
+}
+
+noCityBtn.onclick = () => {
+	inputCity.value = ''
+	startSearch()
+}
+
+inputCity.oninput = () => {
+	startSearch()
 }
