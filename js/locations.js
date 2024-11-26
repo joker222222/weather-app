d413ffac2c7e1201a487718e0fcc8fdc102b952c
joker222222 <live.korpus@mail.ru>
2024-11-26 06:44:05 +0300

    add-aos-and-small-fix

--- js/locations.js
+++ js/locations.js
@@ -1,28 +1,60 @@
+AOS.init()
 let inputCity = document.querySelector('.search-input')
 let appContent = document.querySelectorAll('.content')
+let parentAppContent = document.querySelector('.parent')
 let contentCity = document.querySelectorAll('.content-city-name')
 let noCityText = document.querySelector('.not-found-text-hidden')
 let noCityBtn = document.querySelector('.not-found-btn-hidden')
+const speedOpacity = 0.25
+
+const contentHideFn = contents => {
+	contents.forEach(element => {
+		element.classList.add('content-hidden')
+	})
+}
+
+const contentShowFn = contents => {
+	contents.forEach(element => {
+		element.classList.remove('content-hidden')
+		// element.style.opacity = '0'
+		// let opacity = 0
+		// const increaseOpacity = () => {
+		// 	opacity += 0.05
+		// 	element.style.opacity = opacity
+		// 	if (opacity >= 1) {
+		// 		clearInterval(increaseOpacity)
+		// 	} else {
+		// 		requestAnimationFrame(increaseOpacity)
+		// 	}
+		// }
+		// requestAnimationFrame(increaseOpacity)
+	})
+}
 
 const checkAndSetCity = currentCity => {
 	let countHiddenCity = 0
+	let contentHide = []
+	let contentShow = []
 	for (let i = 0; i < appContent.length; i++) {
+		let content = appContent[i]
 		let cityName = String(
-			appContent[i].querySelector('.content-city-name').textContent
+			content.querySelector('.content-city-name').textContent
 		).toLowerCase()
 		if (cityName.indexOf(currentCity) >= 0) {
-			appContent[i].classList.remove('content-hidden')
+			contentShow.unshift(content)
 		} else {
 			countHiddenCity++
-			appContent[i].classList.add('content-hidden')
+			contentHide.unshift(content)
 		}
 	}
+	contentHideFn(contentHide)
+	contentShowFn(contentShow)
 	return countHiddenCity
 }
 
 const setTextAndBtnNotFound = countHiddenCity => {
 	if (countHiddenCity == appContent.length) {
-		noCityText.textContent = 'Городов не найдено'
+		noCityText.textContent = 'No cities found'
 		noCityText.classList.add('not-found-text')
 		noCityText.classList.remove('not-found-text-hidden')
 
